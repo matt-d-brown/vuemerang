6811cec86d6e7a5303d85e82dd17e3f027c04695
IvanSotelo <isotelo@controlla.com.mx>
2019-04-18 12:20:21 -0600

    deploy: Update Vuemerang

--- dist/components/vmTypeahead/vmTypeaheadItem.vue
+++ dist/components/vmTypeahead/vmTypeaheadItem.vue
@@ -223,10 +223,15 @@ export default {
       }
     },
     putValue(){
-      if(this.value == this.$parent.parent.value){
-        this.$parent.parent.valuex = this.text
+      if (typeof this.value === 'object' && this.value !== null) {
+        if(this.$parent.parent.serializer(this.value) == this.$parent.parent.serializer(this.$parent.parent.value)){
+          this.$parent.parent.valuex = this.text
+        }
+      } else {
+        if(this.value == this.$parent.parent.value){
+          this.$parent.parent.valuex = this.text
+        }
       }
-
     },
     clickOption(){
       if(this.disabledx){
